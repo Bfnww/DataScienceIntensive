298a1036fbd4e139937de540336742c3d5eb3b23
Ruiye Ni <RuiyeNi@users.noreply.github.com>
2016-03-21 17:32:16 -0500

    Added files via upload

---
+++ Machine-Learning/evaluate_poi_identifier.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+
+"""
+    Starter code for the evaluation mini-project.
+    Start by copying your trained/tested POI identifier from
+    that which you built in the validation mini-project.
+
+    This is the second step toward building your POI identifier!
+
+    Start by loading/formatting the data...
+"""
+
+import pickle
+import sys
+sys.path.append("../tools/")
+from feature_format import featureFormat, targetFeatureSplit
+
+data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
+
+### add more features to features_list!
+features_list = ["poi", "salary"]
+
+data = featureFormat(data_dict, features_list)
+labels, features = targetFeatureSplit(data)
+
+
+### your code goes here 
+from sklearn import tree
+from sklearn.cross_validation import train_test_split
+from sklearn import metrics
+X_train,X_test, Y_train,Y_test = train_test_split(features,labels,test_size = 0.3, random_state = 42)
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(X_train,Y_train)
+pred = clf.predict(X_test)
+print sum(pred)
+print len(pred)
+print 1 - sum(pred)*1.0/len(pred)
+print pred[Y_test == True]
+print metrics.accuracy_score(Y_test,pred)
